8ecbffb06860edfcf1909f34f77eea80592d88fe
SuraZz007 <surajdev9886@gmail.com>
2022-08-16 16:41:45 +0530

    component structuring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2021, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2022, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2022, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2022, 5, 12),
+    },
+  ];
   return (
     <div>
-      <h1>Learning React</h1>
-      <ExpenseItem />
+      <h1>Track Expenses</h1>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
